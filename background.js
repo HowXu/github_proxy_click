181a087be083d3ae3bfc9137bdbaa7a1710a5643
HowXu <howxu366@outlook.com>
2024-11-11 20:48:04 +0800

    update: 支持下载源代码zip文件

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ browser.contextMenus.create({
     id: "open-with-moeyy",
     title: "使用Moeyy代理下载",
     contexts: ["link"],
-    documentUrlPatterns: ["*://github.com/*/releases/*"] // 仅在 GitHub Releases 页面生效
+    targetUrlPatterns: ["*://*/*archive*"] // 仅在链接包含 "archive" 时显示
 });
 
 // 监听右键菜单点击事件
